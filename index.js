922aac2b34f3e69ff91100edc7bc86befe391af7
Carlos Estrada <carlos.deg02@hotmail.com>
2020-01-27 21:16:27 -0600

    Finished user create route

--- index.js
+++ index.js
@@ -25,6 +25,54 @@ app.get('/api/users', jsonParser, (req, res) => {
 
 app.get('/api/users-by-id', jsonParser, (req, res) => {
     let id = req.query.id;
+
+    if (id == undefined) {
+        res.statusMessage = "No id given to get users";
+        res.status(406).send();
+    }
+
+    UserController.getById(id)
+        .then(user => {
+            return res.status(200).json(user);
+        })
+        .catch(error => {
+            console.log(error);
+            res.statusMessage = "Database error";
+            return res.status(500).send();
+        });
+});
+
+app.post('/api/create-user', jsonParser, (req, res) => {
+    let firstName, lastName, email, pp;
+
+    firstName = req.body.firstName;
+    lastName = req.body.lastName;
+    email = req.body.email;
+    pp = req.body.profilePicture;
+
+    if (firstName == undefined || lastName == undefined || email == undefined || pp == undefined) {
+        res.statusMessage = "Parameters to create users incomplete";
+        return res.status(406).send();
+    }
+
+    let newUser = {
+        name: {
+            firstName: firstName,
+            lastName: lastName
+        },
+        email: email,
+        profilePicture: pp
+    }
+
+    UserController.create(newUser)
+        .then(nu => {
+            return res.status(201).json(nu);
+        })
+        .catch(error => {
+            console.log(error);
+            res.statusMessage = "Database error";
+            return res.status(500).send();
+        });
 });
 
 let server;

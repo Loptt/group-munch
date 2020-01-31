b6040b66be6899e304f2690af4c4caa50176f4d5
Carlos Estrada <carlos.deg02@hotmail.com>
2020-01-30 21:31:18 -0600

    Added login react

---
+++ client/src/components/Login.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import Container from "react-bootstrap/Container";
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            loginErrors: ""
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        const { email, password } = this.state;
+    
+        axios
+          .post(
+            "http://localhost:3001/api/login",
+            {
+              user: {
+                email: email,
+                password: password
+              }
+            },
+            { withCredentials: true }
+          )
+          .then(response => {
+            if (response.data.logged_in) {
+              this.props.handleSuccessfulAuth(response.data);
+            }
+          })
+          .catch(error => {
+            console.log("login error", error);
+          });
+        event.preventDefault();
+      }
+
+    render() {
+        return (
+            <Container>
+                <h1>Login</h1>
+                <Form>
+                    <Form.Group>
+                        <Form.Control type="email" placeholder="Email"/>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control type="password" placeholder="Password"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        );
+    }
+}
+

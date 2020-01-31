ba8fbeffc8340e46ea8f5dd687e82ed44396d6cc
Carlos Estrada <carlos.deg02@hotmail.com>
2020-01-31 09:16:49 -0600

    Added axios

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 import Container from "react-bootstrap/Container";

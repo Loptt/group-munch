4264e0500163d2b773e705cc1192aa01ae112480
Moisés Fernández <moy-881@hotmail.com>
2020-02-01 17:19:31 -0600

    Added voting event controller and routes

--- models/votingevent.js
+++ models/votingevent.js
@@ -25,5 +25,62 @@ let votingEventCollection = mongoose.Schema({
 let VotingEvent = mongoose.model('voting_events', votingEventCollection);
 
 let VotingEventController = {
-    
+    getAll: function() {
+        return VotingEvent.find()
+            .then(votingEvents => {
+                return votingEvents;
+            })
+            .catch(error => {
+                throw Error(error);
+            });
+    },
+    getById: function(id) {
+        return VotingEvent.findById(id)
+            .then(votingEvent => {
+                return votingEvent;
+            })
+            .catch(error => {
+                throw Error(error);
+            });
+    },
+    getByGroupId: function(id) {
+        return VotingEvent.find({group: id})
+            .then(votingEvent => {
+                return votingEvent;
+            })
+            .catch(error => {
+                throw Error(error);
+            });
+    },
+    create: function(newVotingEvent) {
+        return VotingEvent.create(newVotingEvent)
+            .then(nve => {
+                return nve;
+            })
+            .catch(error => {
+                throw Error(error);
+            });
+    },
+    update: function(id, updatedVotingEvent) {
+        return VotingEvent.findByIdAndUpdate(id, updatedVotingEvent)
+            .then (uve => {
+                return uve;
+            })
+            .catch(error => {
+                throw Error(error);
+            });
+    },
+    delete: function(id) {
+        return VotingEvent.findByIdAndRemove(id)
+            .then(rve => {
+                rve;
+            })
+            .catch(error => {
+                throw Error(error);
+            });
+    }
+}
+
+module.exports = {
+    VotingEventController
 }
